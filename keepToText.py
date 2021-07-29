1d9e08f2d8880fd01509e8f838b9ad6d5ea2ed49
vincekd <vincekd@gmail.com>
2021-07-28 19:52:59 -0500

    Google Takeout Keep Notes to Evernote enex files

--- keepToText.py
+++ keepToText.py
@@ -1,11 +1,12 @@
 from __future__ import print_function
-import sys, glob, os, shutil, zipfile, time, codecs, re, argparse
+import sys, glob, os, shutil, zipfile, time, codecs, re, argparse, json, base64, hashlib
 from zipfile import ZipFile
+from datetime import datetime, timezone
+from PIL import Image
 
-titleCount = 0
-inputPathCount = 0
 indexErrorCount = 0
 fileCount = 0
+jsonExt = re.compile(r"\.json$", re.I)
 
 class InvalidEncoding(Exception):
     def __init__(self, inner):
@@ -16,106 +17,78 @@ def msg(s):
     print(s, file=sys.stderr)
     sys.stderr.flush()
 
-def htmlFileToText(inputPath, outputDir, tag, attrib, attribVal):
-    basename = os.path.basename(inputPath).replace(".html", ".txt")
-    outfname = os.path.join(outputDir, basename)
-    try:
-        with codecs.open(inputPath, "r", "utf-8") as inf, codecs.open(outfname, "w", outputEncoding) as outf:
-            html = inf.read()
-            parser = MyHTMLParser(outf, tag, attrib, attribVal)
-            parser.feed(html)
-
-            parser = MyHTMLParser(outf, "span", "class", "label-name")
-            parser.feed(html)
-    except UnicodeEncodeError as ex:
-        msg("Skipping file " + inputPath + ": " + str(ex))
-    except LookupError as ex:
-        raise InvalidEncoding(ex)
-
-def htmlFileToEnex(inputPath, outputDir, tag, attrib, attribVal, inputDir):
+def jsonFileToEnex(inputPath, outputDir, tag, attrib, attribVal, inputDir):
     basename = os.path.basename(inputPath).replace(".html", ".enex")
     global fileCount
     outfname = os.path.join(outputDir, str(fileCount)+".enex")
-    global titleCount
-    global inputPathCount
-    global indexErrorCount
+
     fileCount += 1
-    println(args.author)
     with codecs.open(inputPath, "r", "utf-8") as inf, codecs.open(outfname, "w", outputEncoding) as outf:
         try:
-            note = extractNoteFromHtmlFile(inputPath)
-
-            # check if there is any title
-            title = "No title"
-            if len(note.title) != 0:
-                title = note.title[0]
-
-            try:
-                # remove line breaks in title
-                originalLen = len(title)
-                title = title.replace('\n', '   ').replace('\r', '   ')
-                afterLen = len(title)
-
-                # remove ampersand
-                title = title.replace('&', '_')
-
-                # edit title if length too long
-                if afterLen >= 250:
-                    note.text = title + note.text
-                    title = title[:251]
-                titleCount += 1
-            except:
-                inputPathCount += 1
-            note.title = title
-
-            # replace line break with <br/>
-            note.text = note.text.replace('\n', '<br/>').replace('\r', '<br/>').replace('&', '_')
-            created = "Today"
-            updated = "Today"
+            note = extractNoteFromJsonFile(inputPath, inputDir)
 
             # enex file template
-            enexXML = Template("""
-                <?xml version="1.0" encoding="UTF-8"?>
-                <!DOCTYPE en-export SYSTEM "http://xml.evernote.com/pub/evernote-export3.dtd">
-                <en-export application="Evernote" version="Evernote">
-                    <note>
-                        <title>${note.title}</title>
-                        <created>${dateTime}</created>
-                        <updated></updated>
-                        <note-attributes>
-                            <author>${args.author}</author>
-                        </note-attributes>
-                        <content>
-                            <![CDATA[<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-                            <!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">
-                            <en-note style="word-wrap: break-word; -webkit-nbsp-mode: space; -webkit-line-break: after-white-space;">
-                                ${note.text}
-                            </en-note>
-                            ]]>
-                        </content>
-                        % for label in note.labels:
-                            <tag>${label}</tag>
-                        % endfor
-                    </note>
-                </en-export>
-            """)
+            enexXML = Template("""<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE en-export SYSTEM "http://xml.evernote.com/pub/evernote-export4.dtd">
+<en-export application="Evernote" version="Evernote">
+    <note>
+        <title>${note.title}</title>
+        <created>${note.datestamp}</created>
+        <updated>${note.datestamp}</updated>
+        <note-attributes>
+            <author>${note.author}</author>
+        </note-attributes>
+        <content>
+            <![CDATA[<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+            <!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">
+            <en-note style="word-wrap: break-word; -webkit-nbsp-mode: space; -webkit-line-break: after-white-space;">${note.text}
+            % for attachment in note.attachments:
+            <en-media style="--en-naturalWidth:${attachment["width"]}; --en-naturalHeight:${attachment["height"]};" hash="${attachment["hash"]}" type="${attachment["mimetype"]}" />
+            % endfor
+            </en-note>
+            ]]>
+        </content>
+        
+        % for attachment in note.attachments:
+        <resource>
+            <data encoding="base64">
+${attachment["data"]}
+            </data>
+            <mime>${attachment["mimetype"]}</mime>
+            <width>${attachment["width"]}</width>
+            <height>${attachment["height"]}</height>
+            <resource-attributes>
+                <file-name>${attachment["filename"]}</file-name>
+                <source-url></source-url>
+            </resource-attributes>
+        </resource>
+        % endfor
+        
+        % for label in note.labels:
+        <tag>${label}</tag>
+        % endfor
+    </note>
+</en-export>
+""")
 
             with codecs.open(outfname, 'w', 'utf-8') as outfile:
                 outfile.write(enexXML.render(note=note))
         except IndexError:
+            msg("index error: " + inputPath)
             indexErrorCount += 1
 
     
         
-def htmlDirToText(inputDir, outputDir, tag, attrib, attribVal):
+def jsonDirToEnex(inputDir, outputDir, tag, attrib, attribVal):
     try_rmtree(outputDir)
     try_mkdir(outputDir)
-    msg("Building text files in {0} ...".format(outputDir))
+    msg("Building enex files in {0} ...".format(outputDir))
     
-    for path in glob.glob(os.path.join(inputDir, "*.html")):
-        htmlFileToEnex(path, outputDir, tag, attrib, attribVal, inputDir)
-        
-    msg("Done.")
+    for path in glob.glob(os.path.join(inputDir, "*.json")):
+        jsonFileToEnex(path, outputDir, tag, attrib, attribVal, inputDir)
+
+    global fileCount
+    msg("Done. Imported %s json files." % fileCount)
     
 def tryUntilDone(action, check):
     ex = None
@@ -150,65 +123,85 @@ def try_mkdir(dir):
     def check(): return os.path.isdir(dir)
     tryUntilDone(act, check)
     
-htmlExt = re.compile(r"\.html$", re.I)
-    
 class Note:
-    def __init__(self, title, text, labels, dtime):
+    def __init__(self, title, text, labels, dtime, attachments):
 
         #self.ctime = parse(heading, parserinfo(dayfirst=True))
-        self.title = title
+        self.title = title or args.defaultTitle
         self.text = text
         self.labels = labels
-
-    def getWsSeparatedLabelString(self):
-        "Return a WS-separated label string suited for import into CintaNotes"
-        labels = []
-        for label in self.labels:
-            label = label.replace(" ", "_")
-            label = label.replace(",", "")
-            labels.append(label)
-        return " ".join(labels)
+        self.datetime = dtime
+        self.datestamp = dtime.strftime("%Y%m%dT%H%M%SZ")
+        self.author = args.author
+        self.attachments = attachments
         
-def extractNoteFromHtmlFile(inputPath):
+def extractNoteFromJsonFile(inputPath, inputDir):
     """
     Extracts the note heading (containing the ctime), text, and labels from
     an exported Keep HTML file
     """
+    global fileCount
 
     with codecs.open(inputPath, 'r', 'utf-8') as myfile:
         data = myfile.read()
+    note = json.loads(data)
+    title = note.get("title", "").strip()
+    # edit title if length too long
+    if len(title) >= 250:
+        note.text = "title: " + title + "\n\n" + note.text
+        title = title[:251]
+    #print("title: " + title)
     
-    tree = etree.HTML(data)
+    text = ""
+    if "listContent" in note:
+        #msg("List content" + title + " - " + str(fileCount - 1))
+        #print(note.get("listContent"))
+        for li in note.get("listContent"):
+            text += "<li>" + li.get("text") + "</li>"
+        text = "<ul>" + text + "</ul>"
+        if title:
+            title = "Checklist: " + title
+    else:
+        text = note.get("textContent", "")
 
+    text = text.strip().replace('\n', '<br/>').replace('\r', '<br/>').replace('&', '&amp;')
+    
+    labels = [t["name"].strip() for t in note.get("labels", [])]
+    dtime = note.get("userEditedTimestampUsec") / 1000 / 1000
+    stime = datetime.utcfromtimestamp(dtime)
 
-    title = []
-    for t in tree.xpath("//div[@class='title']/text()"):
-        title.append(t)
+    attachments = []
+    for attachment in note.get("attachments", []):
+        path = os.path.join(inputDir, attachment["filePath"].replace(".jpeg", ".jpg"))
+        #print(path, attachment["filePath"])
+        with codecs.open(path, "rb") as image_file:
+            data = image_file.read()
+            attachment["data"] = base64.b64encode(data).decode("utf-8")
+            #print(attachment["data"].decode('utf-8'))
+            attachment["filename"] = attachment["filePath"]
+            attachment["hash"] = hashlib.md5(data).hexdigest()
+            attachment["path"] = path
 
-    try:
-        archiveStatus = tree.xpath("//span[@class='archived']")[0]
-        archiveStatus = True
-    except IndexError:
-        archiveStatus = False
-
-    text = "\n".join(tree.xpath("//div[@class='content']/text()"))
-    labels = []
-    for label in tree.xpath("//span[@class='label-name']/text()"):
-        labels.append(label)
-    if archiveStatus:
-        labels.append("Archive")
+            try:
+                img = Image.open(path)
+                attachment["width"] = img.width
+                attachment["height"] = img.height
+            except Exception as e:
+                attachment["width"] = ""
+                attachment["height"] = ""
+                print(e)
         
-    dtime = "Nonono"
-    println(tree.xpath("//span[@class='heading']/text()"))
+        #print(attachment)    
+        attachments.append(attachment)
 
-    return Note(title, text, labels, dtime)
+    return Note(title, text, labels, stime, attachments)
 
-def getHtmlDir(takeoutDir):
-    "Returns first subdirectory beneath takeoutDir which contains .html files"
+def getJsonDir(takeoutDir):
+    "Returns first subdirectory beneath takeoutDir which contains .json files"
     dirs = [os.path.join(takeoutDir, s) for s in os.listdir(takeoutDir)]
     for dir in dirs:
         if not os.path.isdir(dir): continue
-        htmlFiles = [f for f in os.listdir(dir) if htmlExt.search(f)]
+        htmlFiles = [f for f in os.listdir(dir) if jsonExt.search(f)]
         if len(htmlFiles) > 0: return dir
 
 def keepZipToOutput(zipFileName):
@@ -226,13 +219,13 @@ def keepZipToOutput(zipFileName):
     except (IOError, zipfile.BadZipfile) as e:
         sys.exit(e)
 
-    htmlDir = getHtmlDir(takeoutDir)
-    if htmlDir is None: sys.exit("No Keep directory found")
+    jsonDir = getJsonDir(takeoutDir)
+    if jsonDir is None: sys.exit("No Keep directory found")
     
-    msg("Keep dir: " + htmlDir)
+    msg("Keep dir: " + jsonDir)
 
     if args.format == "Evernote":
-        htmlDirToText(inputDir=htmlDir,
+        jsonDirToEnex(inputDir=jsonDir,
             outputDir=os.path.join(zipFileDir, "Text"),
             tag="div", attrib="class", attribVal="content")
         
@@ -254,12 +247,13 @@ def getArgs():
     parser.add_argument("--format", choices=["Evernote"],
         default='Evernote', help="Output Format")
     parser.add_argument("--author", default="Anonymous")
+    parser.add_argument("--defaultTitle", default="")
     global args
     args = parser.parse_args()    
 
 def doImports():
-    global etree
-    from lxml import etree
+##    global etree
+##    from lxml import etree
     global Template
     from mako.template import Template
     global parse, parserinfo
@@ -278,8 +272,6 @@ def main():
         sys.exit(ex)
     except InvalidEncoding as ex:
         sys.exit(ex.inner)
-    global titleCount
-    global inputPathCount
     global indexErrorCount
     global fileCount
 
